3c2548d92dbf202c0fa3d1f219884b15b505592c
shch989 <tankpop11@gmail.com>
2023-08-10 01:17:09 +0900

    리스트추가 post함수 추가

--- server/main.go
+++ server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID    int    `json: "id"`
+	Title string `json: "title"`
+	Done  bool   `json: "done"`
+	Body  string `json: "body"`
+}
 
 func main() {
-	fmt.Println()
+	fmt.Println("Hello world")
+
+	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/ok", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		err := c.BodyParser(todo)
+		if err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":5000"))
+
 }
